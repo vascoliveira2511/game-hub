cf21b0dec1194319e457cbce00b7b8892fc9540e
Vasco Mota de Oliveira <vascoliveira2511@gmail.com>
2024-09-16 21:40:27 +0100

    Build a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -14,8 +15,8 @@ function App() {
       `,
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
