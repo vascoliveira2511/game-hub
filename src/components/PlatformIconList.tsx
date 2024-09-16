548906215ca559ab73350ec32aa37eda511ba0e5
Vasco Mota de Oliveira <vascoliveira2511@gmail.com>
2024-09-16 23:47:29 +0100

    Displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,48 @@
+import {
+  FaWindows,
+  FaXbox,
+  FaPlaystation,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsNintendoSwitch } from "react-icons/bs";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+function PlatformIconList({ platforms }: Props) {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    xbox: FaXbox,
+    playstation: FaPlaystation,
+    ios: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    mac: FaApple,
+    nintendo: BsNintendoSwitch,
+    web: BsGlobe,
+    iphone: MdPhoneIphone,
+    ipad: MdPhoneIphone,
+  };
+
+  return (
+    <HStack marginY={2} spacing={2}>
+      {platforms.map((platform) => (
+        <Icon
+          as={iconMap[platform.slug]}
+          color={"gray.500"}
+          key={platform.id}
+        />
+      ))}
+    </HStack>
+  );
+}
+
+export default PlatformIconList;
