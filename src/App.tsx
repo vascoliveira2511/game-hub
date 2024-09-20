7c66ede706544ba66740852ef842a865749ba885
Vasco Oliveira <vascoliveira2511@gmail.com>
2024-09-20 09:22:41 +0100

    Vercel speed analyzer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
+import {} from "@vercel/speed-insights/next";
 import { useState } from "react";
 import GameGrid from "./components/GameGrid";
 import GameHeading from "./components/GameHeading";
