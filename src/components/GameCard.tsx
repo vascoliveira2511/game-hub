60a2e92a07d11aefffb5691d9b91dd056c4c2f5c
Vasco Mota de Oliveira <vascoliveira2511@gmail.com>
2024-09-21 01:32:42 +0100

    Clickable cards instead of heading

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,32 +12,37 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card height="100%">
-      {/* Makes the card fill the container */}
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody display="flex" flexDirection="column">
-        {/* Top Section */}
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
+    <Link to={`/games/${game.slug}`} style={{ textDecoration: "none" }}>
+      <Card
+        as="article"
+        height="100%"
+        _hover={{ boxShadow: "lg" }}
+        cursor="pointer"
+      >
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody display="flex" flexDirection="column">
+          {/* Top Section */}
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
 
-        {/* Game Name */}
-        <Box flex="1">
-          {/* Takes up remaining space */}
-          <Heading fontSize="xl" noOfLines={2}>
-            <Link to={`/games/${game.slug}`}>{game.name}</Link>
-          </Heading>
-        </Box>
+          {/* Game Name */}
+          <Box flex="1">
+            <Heading fontSize="xl" noOfLines={2}>
+              {game.name}
+            </Heading>
+          </Box>
 
-        {/* Bottom Section */}
-        <HStack justifyContent="flex-end" marginTop={3}>
-          <Emoji rating={game.rating_top} />
-        </HStack>
-      </CardBody>
-    </Card>
+          {/* Bottom Section */}
+          <HStack justifyContent="flex-end" marginTop={3}>
+            <Emoji rating={game.rating_top} />
+          </HStack>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
