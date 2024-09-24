69ad82e3dfb645a373bf6d2f6fdb0823eb29a770
Vasco Oliveira <vascoliveira2511@gmail.com>
2024-09-24 10:07:27 +0100

    Update useGenre.ts

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,7 +1,7 @@
 import useGenres from "./useGenres";
 
-const usePlatform = (id?: number) => {
+const useGenre = (id?: number) => {
   const { data: genres } = useGenres();
   return genres?.results.find((genre) => genre.id === id);
 };
-export default usePlatform;
+export default useGenre;
